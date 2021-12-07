22f4657b6aac10dde33df9eaec6c5c807623cc19
Ara <ara.akopyan@gmail.com>
2021-12-08 00:54:20 +0400

    feat: Elon Mask task, here we have function as a argument situation

--- Elon_Mask.js
+++ Elon_Mask.js
@@ -3,8 +3,8 @@ function printAfter(print) {
     return print()
   }
   // we have const print which function creating only "Elon Mask", it doesn't depend on parameter of this function. 
-  //so we need only to add console.log("Hello, world") in function printAfter. And function print here is as parameter. I guess ^-^.
-  
+  //so we need only to add console.log("Hello, world") in function printAfter. And function print here is as argument. I guess ^-^.
+
   const print = () => console.log('Elon Musk')
   printAfter(print)
  
